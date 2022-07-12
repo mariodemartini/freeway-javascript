9f360bf34cbe88c02587009a841e7d8054972913
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-07-12 13:49:00 -0300

    atualização do freeway

---
+++ ator.js
@@ -0,0 +1,63 @@
+//código do ator
+
+let xAtor = 85;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if (podeMover()){
+      yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], compCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      voltaAtorPosicaoInicial();
+      somDaColisao.play();
+      if (pontosPositivo()){
+        meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function voltaAtorPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60));
+  text(meusPontos, width/5, 27);
+  
+} 
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    voltaAtorPosicaoInicial();
+    somDoPonto.play();
+  }
+}
+
+function pontosPositivo(){
+  return meusPontos > 0;
+}
+
+function podeMover(){
+  return yAtor < 366;
+}
